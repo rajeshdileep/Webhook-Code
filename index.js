c89fe0660c43b99c8d989c246f7269fe0deda87e
hybristraining-rajesh-admin <rajesh.daundkar@somaiya.edu>
2024-05-31 17:45:39 +0530

    Changes done

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3020
 
 app.get('/', (req, res) => {
   res.send('Hello Rajesh D!')
-  res.send(process.env.connection)
+  res.send(console.log(process.env.connection))
 })
 
 app.listen(port, () => {
+  const conn = "Rajesh Ames";
   console.log(`Example app listening on port ${port}`)
   console.log(process.env.connection)
+  console.log(process.env.uname)
+  console.log(process.env.password)
 })
